8191d40341e5b72c0041ba131560660fb1b954b4
Tyler Wong <ty-w@live.com>
2017-01-07 17:47:01 -0800

    Add unit tests for default, mono, and service recipes

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -6,15 +6,20 @@
 
 require 'spec_helper'
 
-describe 'asf::default' do
-  context 'When all attributes are default, on an unspecified platform' do
+describe 'asf::mono' do
+  context 'When all attributes are default' do
     let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
+      runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04')
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
+
+    # it 'includes the mono and service recipes' do
+    #   expect(chef_run).to include_recipe('asf::mono')
+    #   expect(chef_run).to include_recipe('asf::service')
+    # end
   end
 end
